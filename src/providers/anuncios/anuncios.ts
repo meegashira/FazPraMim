0e20de41ed509a3e2ada18b5cee8e1d45213e038
Suzana Oliveira <suzanaoliveira263@gmail.com>
2018-11-05 20:01:32 -0200

    anuncio ok

--- src/providers/anuncios/anuncios.ts
+++ src/providers/anuncios/anuncios.ts
@@ -4,18 +4,18 @@ import 'firebase/database';
 
 @Injectable()
 export class AnunciosProvider {
-  public Client: User;
+  public store: User;
   public anuncio: firebase.database.Reference;
   constructor() {
     firebase.auth().onAuthStateChanged( user => {
       if(user){
-        this.Client = user;
+        this.store = user;
         this.anuncio = firebase.database().ref(`/anuncio`);
         }
     });
   }
 
   createAnuncio(tipoAnuncio:string, NomeAnuncio: string, CategoriaAnuncio: string, ValorAnuncio:string, UnidadeAnuncio:string, DescricaoAnuncio: string): firebase.database.ThenableReference {    
-    return this.anuncio.push({type:tipoAnuncio, name: NomeAnuncio, category: CategoriaAnuncio,price:ValorAnuncio,unidade:UnidadeAnuncio, description: DescricaoAnuncio , seller: "0" })
+    return this.anuncio.push({type:tipoAnuncio, name: NomeAnuncio, category: CategoriaAnuncio,price:ValorAnuncio,unidade:UnidadeAnuncio, description: DescricaoAnuncio , store: "0" })
   }
 }
