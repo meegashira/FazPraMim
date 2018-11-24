d593eaf721f8a62c89a00de705086deba8c67fc3
Anna Kroviski <annakroviski@gmail.com>
2018-11-24 01:52:36 -0200

    pagamento no crédito (simulação)

---
+++ src/pages/pagamentos/pagamentos.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { IonicPage,
+  NavController,
+  Loading,
+  LoadingController,
+  AlertController } from 'ionic-angular';
+
+@IonicPage({
+  name: 'PagamentosPage'
+})
+@Component({
+  selector: 'page-pagamentos',
+  templateUrl: 'pagamentos.html',
+})
+export class PagamentosPage {
+ 
+  
+ 
+
+}
