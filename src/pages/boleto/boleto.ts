a6459e1a0bade0dccb0fe3445a54814399fd7af9
Anna Kroviski <annakroviski@gmail.com>
2018-11-26 23:45:50 -0200

    Revert "pagamento por boleto (simulação)"
    This reverts commit 8a943fe232beee8b2cb53ec407f7c73135f19b30.

--- src/pages/boleto/boleto.ts
+++ src/pages/boleto/boleto.ts
@@ -14,25 +14,6 @@ import { IonicPage,
 })
 export class BoletoPage {
  
-   CriaPDF(): void {
-    var minhaTabela = document.getElementById('tabela').innerHTML;
-    var style = "<style>";
-    style = style + "table {width: 100%;font: 20px Calibri;}";
-    style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
-    style = style + "padding: 2px 3px;text-align: center;}";
-    style = style + "</style>";
-    // CRIA UM OBJETO WINDOW
-    var win = window.open('', '', 'height=700,width=700');
-    win.document.write('<html><head>');
-    win.document.write('<title>Empregados</title>');   // <title> CABEÇALHO DO PDF.
-    win.document.write(style);                                     // INCLUI UM ESTILO NA TAB HEAD
-    win.document.write('</head>');
-    win.document.write('<body>');
-    win.document.write(minhaTabela);                          // O CONTEUDO DA TABELA DENTRO DA TAG BODY
-    win.document.write('</body></html>');
-    win.document.close(); 	                                         // FECHA A JANELA
-    win.print();                                                            // IMPRIME O CONTEUDO
-}
   
  
 
